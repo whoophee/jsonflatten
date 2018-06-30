bd3f127b11347152450f58a7265398175b7cc63b
Raghav Sairam <sairam.r@husky.neu.edu>
2018-06-30 18:31:44 -0400

    Refactored code and fixed edge cases

--- flatten.py
+++ flatten.py
@@ -1,54 +1,122 @@
 import json
-from pprint import pprint
-filename = "restaurants"
+import sys
 
+default_file = "restaurants.json"
 
-class Schema:
+final_data = {}
 
-    def add_undiscovered(self, key, val):
-        if not self.types.get(key):
-            self.types[key] = val
+# obj : json object to be flattened
+# indices : different unique index used to identify current subobject
+# depth : tuple of names traversed in original object to reach subobject
+def flatten_list(obj, indices, depth):
 
-    def generate_types(self, obj, key = ()):
-        if isinstance(obj, dict):
-            self.add_undiscovered(key, "dict")
+    cur_string = '_'.join(depth)
+    if not final_data.get(cur_string):
+        final_data[cur_string] = []
+    current_obj = []
 
-            for childkey, val in obj.items():
-                self.generate_types(val, key + (childkey, ))
+    # Return list as is, if it entirely consists of non-dict/non-list contents
+    if not any(isinstance(i, (list, dict)) for i in obj):
+        return obj
 
-        elif isinstance(obj, list):
-            self.add_undiscovered(key, "list")
-            for val in obj:
-                self.generate_types(val, key)
+    for index, subobj in enumerate(obj):
+        tmp = None
+        # Accomodate mixed arrays and indexing
+        try:
+            id = subobj.get('id')
+            if id:
+                tmp = flatten(subobj, indices + [('id', id)], depth)
+            else:
+                tmp = flatten(subobj, indices + [('__index', index)], depth)
+        # AttributeError implies the list contains non-dict contents
+        # They are not ignored here, to allow reconstruction of original from subfiles.
+        except AttributeError:
+            tmp = flatten(subobj, indices, depth)
+        if tmp:
+            current_obj.append(tmp)
+    final_data[cur_string] += current_obj
+
+def flatten_dict(obj, indices, depth):
+    cur_string = '_'.join(depth)
+    current_obj = {}
+
+    # Handle nested indexing (e.g. multiple nested "id" or "__index" fields)
+    for name, id in indices:
+        if not current_obj.get(name):
+            current_obj[name] = id
         else:
-            self.add_undiscovered(key, "elem")
+            if not isinstance(current_obj[name], list):
+                current_obj[name] = (current_obj[name],)
+            current_obj[name] += (id,)
 
-    def generate_objects(self, obj):
+    if not final_data.get(cur_string):
+        final_data[cur_string] = []
+
+    for field, subobj in obj.items():
+        temp_field = field
+
+        ############################################################################
+        # RENAMING FIELD NAMES WOULD HIGHLY IMPEDE RECONSTRUCTION OF ORIGINAL FILE #
+        ############################################################################
+
+        # truncate last character if current subobject is a list
+        # stopgap measure:
+        # add "__l" suffix to allow easier reconstruction and lesser
+        # chance of overlapping field names.
+
+        if isinstance(subobj, list) and len(field) > 1:
+            temp_field = field[:-1]
+            # temp_field += '__l'
+
+        # Flatten all child content
+        tmp = flatten(subobj, indices, depth + (temp_field,))
+        # Ignore non-trivial children
+        if tmp:
+            current_obj[temp_field] = tmp
+    final_data[cur_string].append(current_obj)
+
+# Recursive function to flatten object
+def flatten(obj, indices = [], depth = ()):
+
+    if not isinstance(obj, (list, dict)):
+        return obj
+
+    if isinstance(obj, list):
+        return flatten_list(obj, indices, depth)
+
+    if isinstance(obj, dict):
+        return flatten_dict(obj, indices, depth)
 
 
-    def __init__(self, json_obj):
-        self.types = {}
-        self.generate_types(json_obj)
-        self.types.pop(())
+def flatten_json(filename):
+    json_obj = None
+    try:
+        with open(filename) as jsonfile:
+            json_obj = json.loads(jsonfile.read())
+    except:
+        print("Error occured in reading file.")
 
-        schema = {}
-        for key, val in self.types.items():
-            if val in ("list", "dict"):
-                schema[key] = {'children': []}
+    flatten(json_obj)
+    final_data.pop('')
 
-            if val is "list":
-
-
-        pprint(schema)
-        # for key, val in self.types.items():
-        #     key.split('_')
-
-
-
-
-
+    for field, subobj in final_data.items():
+        cur_file = "{}.json".format(field)
+        try:
+            outfile = open(cur_file,'x')
+        except:
+            break
+        print("Generated {}".format(cur_file))
+        json.dump(subobj, outfile)
 
 
-with open("{}.json".format(filename)) as jsonfile:
-    json_obj = json.loads(jsonfile.read())
-    s = Schema(json_obj)
+
+if __name__ == '__main__':
+    # Attempt to read file. Default to restaurants.json
+    if len(sys.argv) > 1:
+        try:
+            flatten_json(sys.argv[1])
+        except Exception as e:
+            print(e)
+    else:
+        print("Defaulting to {}".format(default_file))
+        flatten_json(default_file)
